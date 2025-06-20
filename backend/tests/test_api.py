7b790ea4c2861fee9a7acc4577e8c238a7fdd7c5
techut30 <tech.ut30@gmail.com>
2025-06-20 19:49:16 +0530

    Testing API

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,6 +1,6 @@
 from backend.rag.retriever import search
 
-results = search("What does Tate say about women?", top_k=3)
+results = search("What does Tate say about God?", top_k=3)
 for text, video_id in results:
     print(f"From video {video_id}:")
     print(text)
